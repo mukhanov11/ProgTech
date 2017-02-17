aed8dbc1a3fa9db2b01a8d30c024edbcababab80
Almaz Mukhanov <mukhanov2511@gmail.com>
2017-02-17 11:14:44 +0600

    Add files via upload

---
+++ ExceptionTest/ExceptionTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("We are in try");
+                FileStream fs = new FileStream("test.txt", FileMode.OpenOrCreate);
+                Console.WriteLine("After creating filestream");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Some error: " + e);
+            }
+            Console.WriteLine("We are here");
+            Console.ReadKey();
+        }
+    }
+}
